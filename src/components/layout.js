4a3c1df56e083fd0e03795abdb8d4253e5b1eea2
Jonathan Wu <jonathanwu70@gmail.com>
2020-04-15 00:54:18 -0400

    try slugs

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,13 +32,8 @@ const Layout = ({ children }) => (
           {children}
           <footer>
             <div>
-              © 2019, Built by <a href="https://twitter.com/thorwebdev">Thor</a>{' '}
-              with <a href="https://www.gatsbyjs.org">Gatsby</a>
             </div>
             <div>
-              <a href="https://stripe.com">
-                <img src={stripeLogo} alt="Payments powered by Stripe" />
-              </a>
             </div>
           </footer>
         </div>

d6b88d932db14ec605b191b3cd8e6a4425ca7102
Jonathan Wu <jonathanwu70@gmail.com>
2020-04-17 14:09:17 -0400

    added validate payment func

---
+++ functions/validate-payment.js
@@ -0,0 +1,115 @@
+/*const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+
+exports.HandleIPN = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+
+  res.set('Access-Control-Allow-Origin', 'https://google.com');
+  res.set('Access-Control-Allow-Methods', 'GET');
+	listFiles("value-ease").catch(console.error);
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Headers', 'Authorization');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  }
+  else{
+    console.log(req.body);
+    console.log(req.get('content-type'));
+    res.status(200).send(message);
+  }
+
+};
+
+async function listFiles(bucketName) {
+  // Lists files in the bucket
+  const [files] = await storage.bucket(bucketName).getFiles();
+
+  console.log('Files:');
+  files.forEach(file => {
+    console.log(file.name);
+  });
+}*/
+
+/** Production Postback URL */
+const PRODUCTION_VERIFY_URI = "https://ipnpb.paypal.com/cgi-bin/webscr";
+/** Sandbox Postback URL */
+const SANDBOX_VERIFY_URI = "https://ipnpb.sandbox.paypal.com/cgi-bin/webscr";
+
+const sandbox = true;
+
+function getPaypalURI() {
+  return sandbox ? SANDBOX_VERIFY_URI : PRODUCTION_VERIFY_URI;
+}
+
+exports.HandleIPN = (req, res) => {
+
+  let body = req.body;
+
+  let postreq = 'cmd=_notify-validate';
+
+  // Iterate the original request payload object
+  // and prepend its keys and values to the post string
+  Object.keys(body).map((key) => {
+    postreq = `${postreq}&${key}=${body[key]}`;
+    return key;
+  });
+
+  const axios = require('axios');
+  let options = {
+    method: 'POST',
+    url   : getPaypalURI(),
+    headers: {
+      'Content-Length': postreq.length,
+    },
+    encoding: 'utf-8',
+    data: postreq
+  };
+
+  console.log(body)	
+
+  res.sendStatus(200);
+
+  return new Promise((resolve, reject) => {
+    axios(options).then(function (response) {
+      let resBody = response.data;
+      let bodyResult = resBody.substring(0, 8);
+      console.log('bodyResult: ' + bodyResult);
+
+      if (resBody.substring(0, 8) === 'VERIFIED') {
+        return resolve(true);
+      } else if (resBody.substring(0, 7) === 'INVALID') {
+        return reject(new Error('IPN Message is invalid.'));
+      } else {
+        return reject(new Error('Unexpected response body.'));
+      }
+      console.log(response);
+    }).catch(function (error) {
+      return reject(new Error(error));
+    });      
+
+    /* Make a post request to PayPal
+        return request(options, (error, response, resBody) => {
+
+            if (error || response.statusCode !== 200) {
+                reject(new Error(error));
+                return;
+            }
+
+            let bodyResult = resBody.substring(0, 8);
+            console.log('bodyResult: ' + bodyResult);
+
+            // Validate the response from PayPal and resolve / reject the promise.
+            if (resBody.substring(0, 8) === 'VERIFIED') {
+                return resolve(true);
+            } else if (resBody.substring(0, 7) === 'INVALID') {
+                return reject(new Error('IPN Message is invalid.'));
+            } else {
+                return reject(new Error('Unexpected response body.'));
+            }
+        });*/
+
+
+  });
+
+}

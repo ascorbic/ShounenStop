b8e8a9624355237c693e5cb5b3df6490eb71b1d5
Jonathan Wu <jonathanwu70@gmail.com>
2020-04-19 20:32:52 -0400

    try to log

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,10 +3,10 @@ import CMS from 'netlify-cms-app'
 CMS.registerEventListener({
   name: 'postPublish',
   handler: ({ author, entry }) => {
-    console.log(`entry.newRecord:`, entry.newRecord) // true
-    console.log(`entry.collection:`, entry.collection) // 'articles'
-    console.log(`entry.data:`, entry.data) // { ... }
-
+    console.log(`entry.newRecord:`, entry.newRecord); // true
+    console.log(`entry.collection:`, entry.collection); // 'articles'
+    console.log(`entry.data:`, entry.data); // { ... }
+    console.log("OMG WHERE IS THIS");
     if (entry.newRecord && entry.collection === 'articles') {
       // trigger webhook here
     }

029a4d268215995b4d75ac12c1c3955fc449f14b
Jonathan Wu <jonathanwu70@gmail.com>
2020-04-19 21:03:33 -0400

    send post

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,5 @@
-import CMS from 'netlify-cms-app'
+import CMS from 'netlify-cms-app';
+import axios from 'axios';
 
 console.log("where does this even go?");
 
@@ -9,6 +10,12 @@ CMS.registerEventListener({
     console.log(`entry.collection:`, entry.collection); // 'articles'
     console.log(`entry.data:`, entry.data); // { ... }
     console.log("OMG WHERE IS THIS");
+
+    axios.get('https://us-central1-shounenstop.cloudfunctions.net/ValidatePayment')
+    .then(function (response) {
+        console.log(response);
+    });
+
     if (entry.newRecord && entry.collection === 'articles') {
       // trigger webhook here
     }

a6d768b84e0a7e67a9cd587947c41fbbc66747ac
Jonathan Wu <jonathanwu70@gmail.com>
2020-04-19 20:21:39 -0400

    added new postpublish

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,14 @@
 import CMS from 'netlify-cms-app'
 
 CMS.registerEventListener({
-    name: 'postPublish',
-    handler: ({ author, entry }) => console.log(JSON.stringify({ author, data: entry.get('data') })),
-  });
-  
+  name: 'postPublish',
+  handler: ({ author, entry }) => {
+    console.log(`entry.newRecord:`, entry.newRecord) // true
+    console.log(`entry.collection:`, entry.collection) // 'articles'
+    console.log(`entry.data:`, entry.data) // { ... }
+
+    if (entry.newRecord && entry.collection === 'articles') {
+      // trigger webhook here
+    }
+  },
+})

ba3c0fec9a27f9430f82bd26d16595f3d5917b67
Jonathan Wu <jonathanwu70@gmail.com>
2020-04-20 22:45:51 -0400

    add cms and other stuff

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -12,9 +12,9 @@ CMS.registerEventListener({
     console.log("OMG WHERE IS THIS");
 
     axios.get('https://us-central1-shounenstop.cloudfunctions.net/ValidatePayment')
-    .then(function (response) {
-        console.log(response);
-    });
+        .then(function (response) {
+            console.log(response);
+        });
 
     if (entry.newRecord && entry.collection === 'articles') {
       // trigger webhook here

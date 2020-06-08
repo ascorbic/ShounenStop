5bfaf3805d4392d5d1565e10cfb4d06de58d10d4
Jonathan Wu <jonathanwu70@gmail.com>
2020-06-08 01:43:09 -0400

    remove window?

--- src/components/LayoutItems/Layout.js
+++ src/components/LayoutItems/Layout.js
@@ -32,7 +32,7 @@ class Layout extends React.Component {
             <ToastContainer
               closeButton={false}
               closeOnClick
-              onClick={() => {window.location.pathname !== "/cart" && navigate('/cart')}}
+              onClick={() => {navigate('/cart')}}
               transition={Slide}
               limit={3}
               css={toastStyles}

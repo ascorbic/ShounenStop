3c01e317f731c9886ec9f364a0ce5d2afdcb961a
Jonathan Wu <jonathanwu70@gmail.com>
2021-07-17 13:50:52 -0400

    finish script

--- scripts/RemoveEvent.py
+++ scripts/RemoveEvent.py
@@ -3,11 +3,22 @@ import os
 comiketDir = "content/comiket"
 eventId = 'mkb4XfKk_'
 itemFiles = []
+
+itemsToDelete = {}
+
 for filename in os.listdir(comiketDir):
   if(filename.endswith(".md")):
-    file = open(comiketDir+"/"+filename, encoding='utf-8')
+    imageToDelete = ""
+    filePath = comiketDir+"/"+filename
+    file = open(filePath, encoding='utf-8')
     fileBuffer = file.read()
     if eventId in fileBuffer:
-      print(fileBuffer)
-# f = open("content/comiket/s-_4ylpuyyc.md", "r")
-# print(f.read())
+      lines = fileBuffer.split("\n")
+      for line in lines:
+        kvp = line.split(" ")
+        if kvp[0] == 'image:':
+          itemsToDelete[filePath] = kvp[1]
+
+for key in itemsToDelete:
+  # os.remove(key)
+  print(key, itemsToDelete[key])
